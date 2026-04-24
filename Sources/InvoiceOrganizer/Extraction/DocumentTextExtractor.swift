2b72b10d7947db8c5c92a4e5d710141ddfec0507
jsfinn <jeff@paperknifemobile.com>
2026-04-24 12:38:53 -0400

    support japanese receipts

--- Sources/InvoiceOrganizer/Extraction/DocumentTextExtractor.swift
+++ Sources/InvoiceOrganizer/Extraction/DocumentTextExtractor.swift
@@ -189,6 +189,7 @@ private enum VisionInvoiceOCR {
             for (index, image) in images.enumerated() {
                 let request = VNRecognizeTextRequest()
                 request.recognitionLevel = .accurate
+                request.recognitionLanguages = ["ja-JP", "en-US"]
                 request.usesLanguageCorrection = true
 
                 let handler = VNImageRequestHandler(cgImage: image)
